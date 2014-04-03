68ebc72e4dca78d2e7cf294d323ddc20cee4c97f
PGirod@sts-sio-caen.info <PGirod@sts-sio-caen.info@0cd437ee-9718-45a7-55fb-f78ec9ae728c>
2014-04-03 07:40:50 +0000

    ajout des controllers pour l'accueil et la classe Collaborator

---
+++ trunk/pango-scrum/src/net/controller/AccueilController.java
@@ -0,0 +1,20 @@
+package net.controller;
+
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+
+public class AccueilController implements SelectionListener {
+
+	@Override
+	public void widgetDefaultSelected(SelectionEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void widgetSelected(SelectionEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ trunk/pango-scrum/src/net/controller/CollaboratorController.java
@@ -0,0 +1,27 @@
+package net.controller;
+
+import net.vues.VLogin;
+
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+
+public class CollaboratorController implements SelectionListener {
+	private VLogin vLogin;
+
+	public CollaboratorController(VLogin vLogin) {
+		this.vLogin = vLogin;
+	}
+
+	@Override
+	public void widgetDefaultSelected(SelectionEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void widgetSelected(SelectionEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
