3adab22a34025fc6c8ac5fc9ad005c93799e0e6b
PGirod@sts-sio-caen.info <PGirod@sts-sio-caen.info@0cd437ee-9718-45a7-55fb-f78ec9ae728c>
2014-04-03 06:34:34 +0000

    modification des vues (suppression des listeners)

---
+++ trunk/pango-scrum/src/net/vues/VLogin.java
@@ -0,0 +1,111 @@
+package net.vues;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.CLabel;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+
+public class VLogin {
+
+	protected Shell shlLogin;
+	private Text text_login;
+	private Text text_mdp;
+	private Button btnValider;
+	private Button btnAnnuler;
+	private Button btnAdmin;
+
+	public Shell getShlLogin() {
+		return shlLogin;
+	}
+
+	public Text getText_login() {
+		return text_login;
+	}
+
+	public Text getText_mdp() {
+		return text_mdp;
+	}
+
+	public Button getBtnValider() {
+		return btnValider;
+	}
+
+	public Button getBtnAnnuler() {
+		return btnAnnuler;
+	}
+
+	public Button getBtnAdmin() {
+		return btnAdmin;
+	}
+
+	/**
+	 * Open the window.
+	 * 
+	 * @wbp.parser.entryPoint
+	 */
+	public void init() {
+		createContents();
+	}
+
+	/**
+	 * Open the window.
+	 */
+	public void open() {
+		Display display = Display.getDefault();
+		createContents();
+		shlLogin.open();
+		shlLogin.layout();
+		while (!shlLogin.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+	}
+
+	/**
+	 * Create contents of the window.
+	 */
+	protected void createContents() {
+		shlLogin = new Shell();
+		shlLogin.setImage(null);
+		shlLogin.setSize(450, 300);
+		shlLogin.setText("Pango-Scrum");
+		shlLogin.setLayout(null);
+
+		Group grpAuthentification = new Group(shlLogin, SWT.NONE);
+		grpAuthentification.setText("Authentification");
+		grpAuthentification.setBounds(26, 30, 336, 200);
+
+		CLabel lblpassword = new CLabel(grpAuthentification, SWT.NONE);
+		lblpassword.setBounds(42, 93, 76, 26);
+		lblpassword.setText("Password");
+
+		CLabel lbllogin = new CLabel(grpAuthentification, SWT.NONE);
+		lbllogin.setBounds(42, 42, 76, 26);
+		lbllogin.setText("Login");
+
+		text_login = new Text(grpAuthentification, SWT.BORDER);
+		text_login.setBounds(169, 42, 135, 26);
+
+		text_mdp = new Text(grpAuthentification, SWT.BORDER | SWT.PASSWORD);
+		text_mdp.setBounds(169, 93, 135, 26);
+
+		/* Bouton Valider */
+		btnValider = new Button(grpAuthentification, SWT.NONE);
+		btnValider.setBounds(140, 160, 90, 30);
+		btnValider.setText("Valider");
+
+		/* Bouton Annuler */
+		btnAnnuler = new Button(grpAuthentification, SWT.NONE);
+		btnAnnuler.setBounds(236, 160, 90, 30);
+		btnAnnuler.setText("Annuler");
+
+		/* Bouton Admin */
+		btnAdmin = new Button(grpAuthentification, SWT.NONE);
+		btnAdmin.setBounds(28, 160, 90, 30);
+		btnAdmin.setText("Admin");
+	}
+}
