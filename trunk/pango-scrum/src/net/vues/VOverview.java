cc2c408a867f7402aec0c39b0dadac468fb9c4f1
cbeaugrand@sts-sio-caen.info <cbeaugrand@sts-sio-caen.info@0cd437ee-9718-45a7-55fb-f78ec9ae728c>
2014-04-03 09:23:52 +0000

    file overview

--- trunk/pango-scrum/src/net/vues/VOverview.java
+++ trunk/pango-scrum/src/net/vues/VOverview.java
@@ -2,6 +2,8 @@ package net.vues;
 
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.SashForm;
+import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
@@ -52,39 +54,55 @@ public class VOverview {
 		shell = new Shell();
 		shell.setSize(800, 600);
 		shell.setText("SWT Application");
-		shell.setLayout(null);
+		shell.setLayout(new FillLayout(SWT.HORIZONTAL));
 
-		tableViewer = new TableViewer(shell, SWT.BORDER | SWT.FULL_SELECTION);
-		tableToDo = tableViewer.getTable();
-		tableToDo.setBounds(10, 343, 242, 209);
+		SashForm sashFormContainOverview = new SashForm(shell, SWT.VERTICAL);
 
-		TableViewer tableViewer_1 = new TableViewer(shell, SWT.BORDER | SWT.FULL_SELECTION);
-		tableInProgress = tableViewer_1.getTable();
-		tableInProgress.setBounds(270, 343, 242, 209);
+		SashForm sashForm = new SashForm(sashFormContainOverview, SWT.NONE);
 
-		TableViewer tableViewer_2 = new TableViewer(shell, SWT.BORDER | SWT.FULL_SELECTION);
-		tableDone = tableViewer_2.getTable();
-		tableDone.setBounds(532, 343, 242, 209);
+		SashForm sashForm_2 = new SashForm(sashForm, SWT.NONE);
 
-		Label lblToDo = new Label(shell, SWT.NONE);
-		lblToDo.setBounds(33, 322, 55, 15);
+		Label lblSummary = new Label(sashForm_2, SWT.NONE);
+		lblSummary.setText("Summary");
+		sashForm_2.setWeights(new int[] { 1 });
+
+		SashForm sashForm_1 = new SashForm(sashForm, SWT.VERTICAL);
+
+		Label lblSprintRecent = new Label(sashForm_1, SWT.NONE);
+		lblSprintRecent.setText("Sprint recent");
+
+		TableViewer tableViewer_3 = new TableViewer(sashForm_1, SWT.BORDER | SWT.FULL_SELECTION);
+		tableSprintRecent = tableViewer_3.getTable();
+		sashForm_1.setWeights(new int[] { 46, 244 });
+		sashForm.setWeights(new int[] { 1, 1 });
+
+		SashForm sashFormProgression = new SashForm(sashFormContainOverview, SWT.VERTICAL);
+
+		SashForm sashFormTableProgressionLabel = new SashForm(sashFormProgression, SWT.NONE);
+
+		Label lblToDo = new Label(sashFormTableProgressionLabel, SWT.CENTER);
 		lblToDo.setText("To do");
 
-		Label lblInProgress = new Label(shell, SWT.NONE);
-		lblInProgress.setBounds(287, 322, 63, 15);
+		Label lblInProgress = new Label(sashFormTableProgressionLabel, SWT.CENTER);
 		lblInProgress.setText("In progress");
 
-		Label lblDone = new Label(shell, SWT.NONE);
-		lblDone.setBounds(547, 322, 55, 15);
+		Label lblDone = new Label(sashFormTableProgressionLabel, SWT.CENTER);
 		lblDone.setText("Done");
+		sashFormTableProgressionLabel.setWeights(new int[] { 1, 1, 1 });
 
-		TableViewer tableViewer_3 = new TableViewer(shell, SWT.BORDER | SWT.FULL_SELECTION);
-		tableSprintRecent = tableViewer_3.getTable();
-		tableSprintRecent.setBounds(270, 72, 504, 209);
+		SashForm sashFormTableProgression = new SashForm(sashFormProgression, SWT.NONE);
 
-		Label lblSprintRecent = new Label(shell, SWT.NONE);
-		lblSprintRecent.setBounds(287, 38, 90, 15);
-		lblSprintRecent.setText("Sprint recent");
+		tableViewer = new TableViewer(sashFormTableProgression, SWT.BORDER | SWT.FULL_SELECTION);
+		tableToDo = tableViewer.getTable();
+
+		TableViewer tableViewer_1 = new TableViewer(sashFormTableProgression, SWT.BORDER | SWT.FULL_SELECTION);
+		tableInProgress = tableViewer_1.getTable();
+
+		TableViewer tableViewer_2 = new TableViewer(sashFormTableProgression, SWT.BORDER | SWT.FULL_SELECTION);
+		tableDone = tableViewer_2.getTable();
+		sashFormTableProgression.setWeights(new int[] { 1, 1, 1 });
+		sashFormProgression.setWeights(new int[] { 52, 233 });
+		sashFormContainOverview.setWeights(new int[] { 266, 293 });
 
 	}
 }
