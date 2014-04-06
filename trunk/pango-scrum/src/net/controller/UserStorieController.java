0ec3bbd8ebce03ae09be8850192220c08ca087c1
pierrick.hue@sts-sio-caen.info <pierrick.hue@sts-sio-caen.info@0cd437ee-9718-45a7-55fb-f78ec9ae728c>
2014-04-06 14:13:24 +0000

    userStorieController

--- trunk/pango-scrum/src/net/controller/UserStorieController.java
+++ trunk/pango-scrum/src/net/controller/UserStorieController.java
@@ -1,5 +1,35 @@
 package net.controller;
 
-public class UserStorieController {
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+
+import net.vues.VAddUserStorie;
+import net.vues.VLogin;
+import net.vues.VSprint;
+import net.vues.VUserStorie;
+
+public class UserStorieController implements SelectionListener{
+
+	private VAddUserStorie vAddUserStorie;
+
+	public UserStorieController(VAddUserStorie vAddUserStorie) {
+		this.vAddUserStorie = vAddUserStorie;
+	}
+
+	public void init() {
+		vAddUserStorie.getTxtaddUserstorie();
+	}
+
+	@Override
+	public void widgetDefaultSelected(SelectionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void widgetSelected(SelectionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
