508de696a1e09b55d9a4f41064e28a6e0bb58140
pierrick.hue@sts-sio-caen.info <pierrick.hue@sts-sio-caen.info@0cd437ee-9718-45a7-55fb-f78ec9ae728c>
2014-04-06 14:12:56 +0000

    prog à jour

--- trunk/pango-scrum/src/net/controller/Prog.java
+++ trunk/pango-scrum/src/net/controller/Prog.java
@@ -1,6 +1,7 @@
 package net.controller;
 
 import net.vues.VAccueil;
+import net.vues.VAddUserStorie;
 import net.vues.VSprint;
 
 public class Prog {
@@ -22,8 +23,11 @@ public class Prog {
 			new AppController(vSprint);
 			vAccueil = new VAccueil();
 			new AppController(vAccueil);
+			VAddUserStorie vAddUserStorie = new VAddUserStorie();
+			new AppController(vAddUserStorie);
 			vAccueil.open();
 			vSprint.open();
+			vAddUserStorie.open();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
