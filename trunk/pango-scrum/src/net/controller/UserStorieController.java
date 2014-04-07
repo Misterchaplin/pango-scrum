b5d7b91a79e265439fa2ee055bd582c4da3e1b78
pierrick.hue@sts-sio-caen.info <pierrick.hue@sts-sio-caen.info@0cd437ee-9718-45a7-55fb-f78ec9ae728c>
2014-04-07 08:41:06 +0000

    Mise à jour de UserStorie et de la vue

--- trunk/pango-scrum/src/net/controller/UserStorieController.java
+++ trunk/pango-scrum/src/net/controller/UserStorieController.java
@@ -1,15 +1,25 @@
 package net.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.jface.viewers.ArrayContentProvider;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
+import org.hibernate.Query;
+import org.hibernate.Session;
 
+import net.models.Sprint;
+import net.models.Userstory;
+import net.technics.HibernateUtil;
 import net.vues.VAddUserStorie;
+import net.vues.VListCollaborators;
 import net.vues.VLogin;
 import net.vues.VSprint;
 import net.vues.VUserStorie;
 
 public class UserStorieController implements SelectionListener{
-
+	
 	private VAddUserStorie vAddUserStorie;
 
 	public UserStorieController(VAddUserStorie vAddUserStorie) {
@@ -17,7 +27,34 @@ public class UserStorieController implements SelectionListener{
 	}
 
 	public void init() {
-		vAddUserStorie.getTxtaddUserstorie();
+		final List <String> userstorie = new ArrayList<String>();
+		vAddUserStorie.getTableViewer().setContentProvider(new ArrayContentProvider());
+		vAddUserStorie.getTableViewer().setInput(getUserstories());
+		vAddUserStorie.getBtnAddUserStorie().addSelectionListener(new SelectionListener() {
+			@Override
+			public void widgetDefaultSelected(SelectionEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void widgetSelected(SelectionEvent arg0) {
+				String storie = vAddUserStorie.getTxtaddUserstorie().getText();
+				if(storie != ""){
+					userstorie.add(storie);
+				}
+				vAddUserStorie.getTableViewer().setContentProvider(new ArrayContentProvider());
+				vAddUserStorie.getTableViewer().setInput(userstorie);
+			}
+		});
+	}
+
+	private List<Userstory> getUserstories() {
+		Session session = HibernateUtil.getSession();
+		Query query = session.createQuery("FROM Userstory WHERE idStatus= 1");
+		List<Userstory> userStories = query.list();
+		return userStories;
+		
 	}
 
 	@Override
