d5cf375ffb2412d6915dcd1b8e834f9c48d6aae3
PGirod@sts-sio-caen.info <PGirod@sts-sio-caen.info@0cd437ee-9718-45a7-55fb-f78ec9ae728c>
2014-04-07 07:18:07 +0000

    mise à jour de AccueilController et AppController

--- trunk/pango-scrum/src/net/controller/AppController.java
+++ trunk/pango-scrum/src/net/controller/AppController.java
@@ -1,42 +1,46 @@
 package net.controller;
 
 import net.models.Collaborator;
+import net.technics.HibernateUtil;
 import net.vues.VAccueil;
 import net.vues.VAddUserStorie;
 import net.vues.VSprint;
 
+import org.hibernate.Session;
+
 public class AppController {
-	//récupérer l'utilisateur actif
+	// utilisateur connectÃ©
 	private static Collaborator activeUser = new Collaborator();
+	// session
+	private static Session session = HibernateUtil.getSession();
+
+	public static Session getSession() {
+		return session;
+	}
 
 	public static Collaborator getActiveUser() {
 		return activeUser;
 	}
 
-	public static void setActiveUser(Collaborator collaborators) {
-		activeUser = collaborators;
+	public static void setActiveUser(Collaborator collaborator) {
+		activeUser = collaborator;
 	}
 
 	public AppController(VAccueil vAccueil) {
 		AccueilController accueilController = new AccueilController(vAccueil);
 		vAccueil.init();
 		accueilController.init();
-		
 	}
+
 	public AppController(VSprint vSprint) {
 		SprintController sprintController = new SprintController(vSprint);
 		vSprint.init();
 		sprintController.init();
 	}
-	public AppController(VAddUserStorie vAddUserStorie){
+
+	public AppController(VAddUserStorie vAddUserStorie) {
 		UserStorieController userStorieController = new UserStorieController(vAddUserStorie);
 		vAddUserStorie.init();
 		userStorieController.init();
 	}
-
-	public static void setSelectedCollaborator(Collaborator selectedCollaborator) {
-		// TODO Auto-generated method stub
-		
-	}
-	
 }
