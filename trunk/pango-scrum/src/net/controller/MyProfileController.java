bfed502a788c67970ffd52ddeed110dd16c942ce
PGirod@sts-sio-caen.info <PGirod@sts-sio-caen.info@0cd437ee-9718-45a7-55fb-f78ec9ae728c>
2014-04-07 08:01:15 +0000

    ajout de la vue MyProfile

---
+++ trunk/pango-scrum/src/net/controller/MyProfileController.java
@@ -0,0 +1,16 @@
+package net.controller;
+
+import net.vues.VMyProfile;
+
+public class MyProfileController {
+
+	private VMyProfile vMyProfile;
+
+	public MyProfileController(VMyProfile vMyProfile) {
+		this.vMyProfile = vMyProfile;
+	}
+
+	public void init() {
+
+	}
+}
