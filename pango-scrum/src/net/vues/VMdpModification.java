3f51fbf2ef88b0678eb3dc950d52d6cf89a4d73b
PGirod <PGirod@sts-sio-caen.info>
2014-04-07 10:12:12 +0000

    ajout de la fonctionnalité de modification du mot de passe de l'utilisateur connecté

---
+++ pango-scrum/src/net/vues/VMdpModification.java
@@ -0,0 +1,58 @@
+package net.vues;
+
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+
+public class VMdpModification {
+
+	protected Shell shell;
+
+	/**
+	 * Launch the application.
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+			VMdpModification window = new VMdpModification();
+			window.open();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Open the window.
+	 */
+	public void open() {
+		Display display = Display.getDefault();
+		createContents();
+		shell.open();
+		shell.layout();
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+	}
+
+	/**
+	 * Open the window.
+	 * 
+	 * @wbp.parser.entryPoint
+	 */
+	public void init() {
+		createContents();
+	}
+
+	/**
+	 * Create contents of the window.
+	 */
+	protected void createContents() {
+		shell = new Shell();
+		shell.setSize(450, 300);
+		shell.setText("SWT Application");
+
+	}
+
+}
