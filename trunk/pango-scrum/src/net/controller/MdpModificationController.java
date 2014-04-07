6eafde3def33ebea8da511f3417cc1bef5250aa0
PGirod@sts-sio-caen.info <PGirod@sts-sio-caen.info@0cd437ee-9718-45a7-55fb-f78ec9ae728c>
2014-04-07 10:12:12 +0000

    ajout de la fonctionnalité de modification du mot de passe de l'utilisateur connecté

---
+++ trunk/pango-scrum/src/net/controller/MdpModificationController.java
@@ -0,0 +1,16 @@
+package net.controller;
+
+import net.vues.VMdpModification;
+
+public class MdpModificationController {
+
+	private VMdpModification vMdpModification;
+
+	public MdpModificationController(VMdpModification vMdpModification) {
+		this.vMdpModification = vMdpModification;
+	}
+
+	public void init() {
+
+	}
+}
