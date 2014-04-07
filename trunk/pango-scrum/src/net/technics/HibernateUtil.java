144145a64a8aa215f1265dbf6160739fb867328a
PGirod@sts-sio-caen.info <PGirod@sts-sio-caen.info@0cd437ee-9718-45a7-55fb-f78ec9ae728c>
2014-04-07 06:35:52 +0000

    ajout d'une classe DAO sur la classe Collaborator

--- trunk/pango-scrum/src/net/technics/HibernateUtil.java
+++ trunk/pango-scrum/src/net/technics/HibernateUtil.java
@@ -6,18 +6,18 @@ import org.hibernate.cfg.AnnotationConfiguration;
 
 public class HibernateUtil {
 
-	private static final SessionFactory	sessionFactory	= buildSessionFactory();
+	private static final SessionFactory sessionFactory = buildSessionFactory();
 
 	private static SessionFactory buildSessionFactory() {
 		try {
-			
 			// Create the SessionFactory from hibernate.cfg.xml
-            return new AnnotationConfiguration().configure().buildSessionFactory();
+			return new AnnotationConfiguration().configure().buildSessionFactory();
 
 		} catch (Throwable ex) {
 			// Make sure you log the exception, as it might be swallowed
 			ex.printStackTrace();
-			//System.err.println("Initial SessionFactory creation failed." + ex);
+			// System.err.println("Initial SessionFactory creation failed." +
+			// ex);
 			throw new ExceptionInInitializerError(ex);
 		}
 	}
