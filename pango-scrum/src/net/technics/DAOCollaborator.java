5e15a7595c0aef4edd699801afb0dc14daeb210c
PGirod <PGirod@sts-sio-caen.info>
2014-04-08 15:26:13 +0000

    modification affectation collaborateurs

--- pango-scrum/src/net/technics/DAOCollaborator.java
+++ pango-scrum/src/net/technics/DAOCollaborator.java
@@ -1,9 +1,12 @@
 package net.technics;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import net.controller.AppController;
+import net.controller.ProductController;
 import net.models.Collaborator;
+import net.models.Playrole;
 
 public class DAOCollaborator {
 
@@ -16,5 +19,22 @@ public class DAOCollaborator {
 		org.hibernate.Query query = AppController.session.createQuery("from Collaborator");
 		List<Collaborator> lesCollaborateurs = query.list();
 		return lesCollaborateurs;
+	}
+
+	public static List<Collaborator> getAffectedCollaborators() {
+		List<Collaborator> lesCollaborateursAffectes = new ArrayList<>();
+		org.hibernate.Query query = AppController.session.createQuery("from Playrole where idProduct = " + ProductController.getSelectedProduct().getId());
+		List<Playrole> lesRolesJoues = query.list();
+		for (Playrole playrole : lesRolesJoues) {
+			Collaborator aCollaborator = (Collaborator) AppController.session.get(Collaborator.class, playrole.getCollaborator().getId());
+			lesCollaborateursAffectes.add(aCollaborator);
+		}
+		return lesCollaborateursAffectes;
+	}
+
+	public static List<Collaborator> getUnaffectedCollaborators() {
+		org.hibernate.Query query = AppController.session.createQuery("from Collaborator where id not in (select id from Collaborator inner join Playrole on Collaborator.id = Playrole.idCollaborator where idProdcut =" + ProductController.getSelectedProduct().getId());
+		List<Collaborator> lesCollaborateursNonAffectes = query.list();
+		return lesCollaborateursNonAffectes;
 	}
 }
--- pango-scrum/src/net/vues/VAffectationCollaborator.java
+++ pango-scrum/src/net/vues/VAffectationCollaborator.java
@@ -1,22 +1,32 @@
 package net.vues;
 
+import net.technics.CollaboratorTvProvider;
+import net.technics.DAOCollaborator;
+
+import org.eclipse.jface.layout.TableColumnLayout;
+import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.ColumnWeightData;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableColumn;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.wb.swt.SWTResourceManager;
 
 public class VAffectationCollaborator {
 
 	protected Shell shell;
-	private Text text;
-	private Text text_1;
+	private Text txtProjet;
+	private Text txtScrumMaster;
 	private Table table;
-	private Table table_1;
+	private Table tableAffectedCollaborators;
+	private TableColumnLayout tLayout;
 
 	/**
 	 * Launch the application.
@@ -72,27 +82,39 @@ public class VAffectationCollaborator {
 		lblCollaborateur.setBounds(10, 203, 163, 15);
 		lblCollaborateur.setText("Collaborateurs affectés :");
 
-		text = new Text(shell, SWT.BORDER);
-		text.setBounds(111, 42, 174, 21);
+		txtProjet = new Text(shell, SWT.BORDER);
+		txtProjet.setBounds(111, 42, 174, 21);
 
-		text_1 = new Text(shell, SWT.BORDER);
-		text_1.setBounds(10, 123, 275, 21);
+		txtScrumMaster = new Text(shell, SWT.BORDER);
+		txtScrumMaster.setBounds(10, 123, 275, 21);
 
 		TableViewer tableViewer = new TableViewer(shell, SWT.BORDER | SWT.FULL_SELECTION);
 		table = tableViewer.getTable();
 		table.setBounds(621, 224, 377, 434);
 
-		Button btnNewButton = new Button(shell, SWT.NONE);
-		btnNewButton.setBounds(291, 42, 174, 25);
-		btnNewButton.setText("Revenir à la liste des projets");
+		Button btnReturnListProject = new Button(shell, SWT.NONE);
+		btnReturnListProject.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+			}
+		});
+		btnReturnListProject.setBounds(291, 42, 174, 25);
+		btnReturnListProject.setText("Revenir à la liste des projets");
 
 		Button btnNewButton_1 = new Button(shell, SWT.NONE);
-		btnNewButton_1.setBounds(292, 123, 95, 25);
+		btnNewButton_1.setBounds(291, 121, 95, 25);
 		btnNewButton_1.setText("Définir");
 
-		TableViewer tableViewer_1 = new TableViewer(shell, SWT.BORDER | SWT.FULL_SELECTION);
-		table_1 = tableViewer_1.getTable();
-		table_1.setBounds(10, 224, 377, 434);
+		TableViewer tvAffectedCollaborators = new TableViewer(shell, SWT.BORDER | SWT.FULL_SELECTION);
+		tableAffectedCollaborators = tvAffectedCollaborators.getTable();
+		tableAffectedCollaborators.setBounds(10, 224, 377, 434);
+		tableAffectedCollaborators.setHeaderVisible(true);
+		tableAffectedCollaborators.setLinesVisible(true);
+		tvAffectedCollaborators.setContentProvider(new ArrayContentProvider());
+		tvAffectedCollaborators.setLabelProvider(new CollaboratorTvProvider());
+		tvAffectedCollaborators.setInput(DAOCollaborator.getAffectedCollaborators());
+
+		createColumn(tableAffectedCollaborators, "Nom", 1);
 
 		Button btnNewButton_2 = new Button(shell, SWT.NONE);
 		btnNewButton_2.setBounds(453, 246, 75, 25);
@@ -106,5 +128,11 @@ public class VAffectationCollaborator {
 		lblNewLabel.setBounds(621, 203, 179, 15);
 		lblNewLabel.setText("Collaborateurs non affectés :");
 
+	}
+
+	public void createColumn(Table table, String caption, int weight) {
+		TableColumn col = new TableColumn(table, SWT.NONE);
+		col.setText(caption);
+		tLayout.setColumnData(col, new ColumnWeightData(weight));
 	}
 }
