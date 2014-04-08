5bc75a465b93e9039ab7a7204b0e6ead6553deba
PGirod@sts-sio-caen.info <PGirod@sts-sio-caen.info@0cd437ee-9718-45a7-55fb-f78ec9ae728c>
2014-04-08 14:34:07 +0000

    ajout produit sélectionné dans ProductController

--- trunk/pango-scrum/src/net/controller/ProductController.java
+++ trunk/pango-scrum/src/net/controller/ProductController.java
@@ -1,5 +1,17 @@
 package net.controller;
 
+import net.models.Product;
+
 public class ProductController {
+	// produit sélectionné
+	private Product selectedProduct = (Product) AppController.session.get(Product.class, 3);
+
+	public Product getSelectedProduct() {
+		return selectedProduct;
+	}
+
+	public void setSelectedProduct(Product selectedProduct) {
+		this.selectedProduct = selectedProduct;
+	}
 
 }
