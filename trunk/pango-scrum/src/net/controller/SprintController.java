9cf388400f023dcae3a8d2315a44fab87c051b4f
mdonge@sts-sio-caen.info <mdonge@sts-sio-caen.info@0cd437ee-9718-45a7-55fb-f78ec9ae728c>
2014-04-10 09:32:18 +0000

    Modification de l'ajout des sprints et de l'interface

--- trunk/pango-scrum/src/net/controller/SprintController.java
+++ trunk/pango-scrum/src/net/controller/SprintController.java
@@ -2,8 +2,11 @@ package net.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import net.models.Sprint;
+import net.technics.DAOSprint;
 import net.vues.VSprint;
+
 import org.eclipse.jface.viewers.ArrayContentProvider;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
@@ -11,22 +14,37 @@ import org.hibernate.Query;
 
 public class SprintController implements SelectionListener {
 	private VSprint vSprint;
+	private List<Sprint> sprints;
 
 	public SprintController(VSprint vSprint) {
 		this.vSprint = vSprint;
 	}
 
 	public void init() {
-		List<Sprint> sprints = new ArrayList<Sprint>();
+		sprints = new ArrayList<Sprint>();
 		sprints = getSprint();
 		vSprint.getTvSprint().setContentProvider(new ArrayContentProvider());
 		vSprint.getTvSprint().setInput(sprints);
+		vSprint.getBtAddSprint().addSelectionListener(new SelectionListener() {
+			
+			@Override
+			public void widgetSelected(SelectionEvent evt) {
+				sprints.add(DAOSprint.addSprint(vSprint));
+				vSprint.getTvSprint().refresh();
+				
+				
+			}
+			
+			@Override
+			public void widgetDefaultSelected(SelectionEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+			
+		
 
 	}
-
-
-
-
 
 	// chargement d'un product , en attendant la partie d'Anthony
 	private List<Sprint> getSprint() {
--- trunk/pango-scrum/src/net/vues/VSprint.java
+++ trunk/pango-scrum/src/net/vues/VSprint.java
@@ -1,9 +1,12 @@
 package net.vues;
 
+import java.util.GregorianCalendar;
+
 import net.technics.ColDebLP;
 import net.technics.ColFinLP;
 import net.technics.ColLbProvider;
 import net.technics.ColSprintLP;
+
 import org.eclipse.jface.layout.TableColumnLayout;
 import org.eclipse.jface.viewers.ColumnWeightData;
 import org.eclipse.jface.viewers.TableViewer;
@@ -11,6 +14,7 @@ import org.eclipse.jface.viewers.TableViewerColumn;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.DateTime;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Table;
@@ -21,6 +25,8 @@ import org.eclipse.swt.layout.FormData;
 import org.eclipse.swt.layout.FormAttachment;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
 
 
 public class VSprint {
@@ -28,13 +34,39 @@ public class VSprint {
 
 	protected Shell shellSprint;
 	private Button btAddSprint;
-	
-
 	private Table tableSprint;
 	private TableViewer tvSprint;
 	private TableColumnLayout tLayout;
+	private Text newNameSprint;
+	private DateTime dateDebut;
+	private DateTime dateFin;
+	private GregorianCalendar gc;
 	
 	
+	public Text getnewNameSprint() {
+		return newNameSprint;
+	}
+
+	public void setnewNameSprint(Text text) {
+		this.newNameSprint = text;
+	}
+
+	public DateTime getDateDebut() {
+		return dateDebut;
+	}
+
+	public void setDateDebut(DateTime dateDebut) {
+		this.dateDebut = dateDebut;
+	}
+
+	public DateTime getDateFin() {
+		return dateFin;
+	}
+
+	public void setDateFin(DateTime dateFin) {
+		this.dateFin = dateFin;
+	}
+
 	public Button getBtAddSprint() {
 		return btAddSprint;
 	}
@@ -111,7 +143,7 @@ public class VSprint {
         Composite composite = new Composite(shellSprint, SWT.NONE);
         FormData fd_composite = new FormData();
         fd_composite.bottom = new FormAttachment(0, 562);
-        fd_composite.right = new FormAttachment(0, 784);
+        fd_composite.right = new FormAttachment(0, 766);
         fd_composite.top = new FormAttachment(0);
         fd_composite.left = new FormAttachment(0);
         composite.setLayoutData(fd_composite);
@@ -121,13 +153,13 @@ public class VSprint {
 	    
 	    
 	    Group GrpSprint = new Group(composite, SWT.NONE);
+	    GrpSprint.setSize(120, 200);
 	    GrpSprint.setToolTipText("");
 	    GrpSprint.setText("Planifié");
-	    GrpSprint.setLayout(null);
+	    GrpSprint.setLayout(new FillLayout(SWT.VERTICAL));
 	    
 	    tvSprint = new TableViewer(GrpSprint, SWT.BORDER | SWT.FULL_SELECTION);
 	    tableSprint = tvSprint.getTable();
-	    tableSprint.setBounds(0, 28, 778, 263);
 	    tableSprint.getParent().setLayout(layout);
 	    
   
@@ -136,39 +168,97 @@ public class VSprint {
 	    layout.setColumnData(TcLblSprint.getColumn(), new ColumnWeightData(1));
 	    TcLblSprint.setLabelProvider(new ColSprintLP());
 	    
-	    TableViewerColumn dateDebut= new TableViewerColumn(tvSprint, SWT.NONE);
-	    dateDebut.getColumn().setText("Date de début");
-	    layout.setColumnData(dateDebut.getColumn(), new ColumnWeightData(1));
-	    dateDebut.setLabelProvider(new ColDebLP());
+	    TableViewerColumn cdateDebut= new TableViewerColumn(tvSprint, SWT.NONE);
+	    cdateDebut.getColumn().setText("Date de début");
+	    layout.setColumnData(cdateDebut.getColumn(), new ColumnWeightData(1));
+	    cdateDebut.setLabelProvider(new ColDebLP());
 	    	    	    
-	    TableViewerColumn dateFin= new TableViewerColumn(tvSprint, SWT.NONE);
-	    dateFin.getColumn().setText("Date de fin");
-	    layout.setColumnData(dateFin.getColumn(), new ColumnWeightData(1));
-	    dateFin.setLabelProvider(new ColFinLP());
-	    
-	    
-	    TableViewerColumn action= new TableViewerColumn(tvSprint, SWT.NONE);
-	    action.getColumn().setText("Actions");
-	    layout.setColumnData(action.getColumn(), new ColumnWeightData(1));
-	    action.setLabelProvider(new ColLbProvider(this));
-
+	    TableViewerColumn cdateFin= new TableViewerColumn(tvSprint, SWT.NONE);
+	    cdateFin.getColumn().setText("Date de fin");
+	    layout.setColumnData(cdateFin.getColumn(), new ColumnWeightData(1));
+	    cdateFin.setLabelProvider(new ColFinLP());
 	    
 	    tableSprint.setHeaderVisible(true);
 	    tableSprint.setLinesVisible(true);
 	    
+	    Group grpAjouterUnSprint = new Group(composite, SWT.SHADOW_OUT);
+	    grpAjouterUnSprint.setText("Ajouter un sprint");
+	    grpAjouterUnSprint.setLayout(new FormLayout());
+	    
+	    Label lblNom = new Label(grpAjouterUnSprint, SWT.NONE);
+	    FormData fd_lblNom = new FormData();
+	    fd_lblNom.left = new FormAttachment(0, 10);
+	    lblNom.setLayoutData(fd_lblNom);
+	    lblNom.setText("Nom :");
+	    
+	    newNameSprint = new Text(grpAjouterUnSprint, SWT.BORDER);
+	    fd_lblNom.right = new FormAttachment(newNameSprint, -19);
+	    FormData fd_text = new FormData();
+	    fd_text.top = new FormAttachment(lblNom, -3, SWT.TOP);
+	    fd_text.left = new FormAttachment(0, 78);
+	    fd_text.right = new FormAttachment(100, -567);
+	    newNameSprint.setLayoutData(fd_text);
+	    
+	    Label lblDateDeDbut = new Label(grpAjouterUnSprint, SWT.NONE);
+	    fd_lblNom.bottom = new FormAttachment(100, -219);
+	    FormData fd_lblDateDeDbut = new FormData();
+	    fd_lblDateDeDbut.left = new FormAttachment(lblNom, 0, SWT.LEFT);
+	    lblDateDeDbut.setLayoutData(fd_lblDateDeDbut);
+	    lblDateDeDbut.setText("Date de début");
+	    
+	    dateDebut = new DateTime(grpAjouterUnSprint,SWT.DATE);
+	    FormData fd_dateDebut = new FormData();
+	    fd_dateDebut.top = new FormAttachment(newNameSprint, 29);
+	    fd_dateDebut.right = new FormAttachment(newNameSprint, 0, SWT.RIGHT);
+	    dateDebut.setLayoutData(fd_dateDebut);
+	    dateDebut.setSize(50, 10);
+	    
+	    
+	    Label lblDateDeFin = new Label(grpAjouterUnSprint, SWT.NONE);
+	    fd_lblDateDeDbut.bottom = new FormAttachment(lblDateDeFin, -26);
+	    FormData fd_lblDateDeFin = new FormData();
+	    fd_lblDateDeFin.top = new FormAttachment(0, 117);
+	    fd_lblDateDeFin.left = new FormAttachment(0, 10);
+	    lblDateDeFin.setLayoutData(fd_lblDateDeFin);
+	    lblDateDeFin.setText("Date de fin");
+	    
+	    dateFin = new DateTime(grpAjouterUnSprint,SWT.DATE);
+	    fd_dateDebut.bottom = new FormAttachment(dateFin, -20);
+	    fd_dateDebut.left = new FormAttachment(dateFin, 0, SWT.LEFT);
+	    fd_lblDateDeFin.right = new FormAttachment(dateFin, -28);
+	    FormData fd_dateFin = new FormData();
+	    fd_dateFin.left = new FormAttachment(0, 101);
+	    fd_dateFin.top = new FormAttachment(0, 117);
+	    dateFin.setLayoutData(fd_dateFin);
+
+	    
 	    Image imAdd=new Image(Display.getCurrent(), getClass().getResourceAsStream("/net/images/addButton.png"));
 	    btAddSprint = new Button(shellSprint, SWT.NONE);
 	    FormData fd_btnNewButton = new FormData();
-	    fd_btnNewButton.top = new FormAttachment(0, 148);
+	    fd_btnNewButton.top = new FormAttachment(0, 81);
 	    fd_btnNewButton.right = new FormAttachment(100);
 	    btAddSprint.setLayoutData(fd_btnNewButton);
 	    btAddSprint.setText("Ajouter Sprint");
 	    btAddSprint.setImage(imAdd);	    
+	    
+	    Button btnModifierSprint = new Button(shellSprint, SWT.NONE);
+	    FormData fd_btnModifierSprint = new FormData();
+	    fd_btnModifierSprint.right = new FormAttachment(btAddSprint, 0, SWT.RIGHT);
+	    fd_btnModifierSprint.top = new FormAttachment(btAddSprint, 24);
+	    fd_btnModifierSprint.left = new FormAttachment(btAddSprint, 0, SWT.LEFT);
+	    btnModifierSprint.setLayoutData(fd_btnModifierSprint);
+	    btnModifierSprint.setText("Modifier sprint");
+	    
+	    Button btnSupprimer = new Button(shellSprint, SWT.NONE);
+	    FormData fd_btnSupprimer = new FormData();
+	    fd_btnSupprimer.right = new FormAttachment(btAddSprint, 0, SWT.RIGHT);
+	    fd_btnSupprimer.top = new FormAttachment(btnModifierSprint, 25);
+	    fd_btnSupprimer.left = new FormAttachment(btAddSprint, 0, SWT.LEFT);
+	    btnSupprimer.setLayoutData(fd_btnSupprimer);
+	    btnSupprimer.setText("Supprimer");
 	    composite.layout(true, true);
 	
-			
+	  
 		}
-
-	
 }
 
