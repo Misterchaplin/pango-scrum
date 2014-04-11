33c756a8247396bb9d18181f336af40ab8996a43
PGirod <PGirod@sts-sio-caen.info>
2014-04-11 14:56:26 +0000

    modification de Prog : ajout d'une variable vAffectationCollaborator

--- pango-scrum/src/net/controller/Prog.java
+++ pango-scrum/src/net/controller/Prog.java
@@ -1,13 +1,13 @@
 package net.controller;
 
 import net.vues.VAccueil;
-import net.vues.VAddUserStorie;
+import net.vues.VAffectationCollaborator;
 import net.vues.VOverview;
-import net.vues.VSprint;
 
 public class Prog {
 
 	public static VAccueil vAccueil;
+	public static VAffectationCollaborator vAffectationCollaborator;
 
 	/**
 	 * Launch the application.
@@ -17,15 +17,20 @@ public class Prog {
 	public static void main(String[] args) {
 
 		try {
-			// ajout de vSprint pour tester la partie sprint sans avoir � se
+			// ajout de vSprint pour tester la partie sprint sans avoir à se
 			// connecter ne pas effacer svp...
 
+			vAffectationCollaborator = new VAffectationCollaborator();
+			new AppController(vAffectationCollaborator);
 			vAccueil = new VAccueil();
 			new AppController(vAccueil);
 			VOverview vOverview = new VOverview();
 			new AppController(vOverview);
 			vAccueil.open();
+			vAffectationCollaborator.open();
 			vOverview.open();
+			// vSprint.open();
+			// vAddUserStorie.open();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
