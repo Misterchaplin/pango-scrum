5c8dcd2aede9c06b5f632db09783c98fc0b0a00a
pierrick.hue <pierrick.hue@sts-sio-caen.info>
2014-06-08 13:41:51 +0000

    Ajout du message d'erreur et de la validation par la touche entrée

--- pango-scrum/src/net/controller/LoginController.java
+++ pango-scrum/src/net/controller/LoginController.java
@@ -4,6 +4,8 @@ import net.models.Collaborator;
 import net.vues.VLogin;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.KeyEvent;
+import org.eclipse.swt.events.KeyListener;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.widgets.MessageBox;
@@ -34,21 +36,6 @@ public class LoginController implements SelectionListener {
 	}
 
 	public void init() {
-		// bouton admin
-		vLogin.getBtnAdmin().addSelectionListener(new SelectionListener() {
-
-			@Override
-			public void widgetSelected(SelectionEvent arg0) {
-				vLogin.getText_login().setText("admin");
-				vLogin.getText_mdp().setText("admin");
-			}
-
-			@Override
-			public void widgetDefaultSelected(SelectionEvent arg0) {
-				// TODO Auto-generated method stub
-
-			}
-		});
 		// bouton valider
 		vLogin.getBtnValider().addSelectionListener(new SelectionListener() {
 
@@ -62,7 +49,7 @@ public class LoginController implements SelectionListener {
 					// fermeture de la bo√Æte de login
 					vLogin.getShlLogin().close();
 					AccueilController.vAccueil.getItemMonProfil().setText("Mon profil");
-					AccueilController.vAccueil.getItemConnexion().setText("D√©connexion");
+					AccueilController.vAccueil.getItemConnexion().setText("DÈconnexion");
 					AccueilController.vAccueil.getItemMonProfil().setEnabled(true);
 					AccueilController.vAccueil.getItemLogin().setText(AppController.getActiveUser().getLogin());
 					AccueilController.vAccueil.getGrpProduits().setVisible(true);
@@ -75,29 +62,19 @@ public class LoginController implements SelectionListener {
 						AccueilController.vAccueil.getBtnProducts().setVisible(false);
 						AccueilController.vAccueil.getBtnCollaborators().setVisible(false);
 						AccueilController.vAccueil.getGrpCollaborateurs().setVisible(false);
-						AccueilController.vAccueil.getGrpCollaborateurs().setText("D√©tail du produit");
+						AccueilController.vAccueil.getGrpCollaborateurs().setText("DÈtail du produit");
 						AccueilController.vAccueil.getTableMesProjets().setVisible(true);
 					}
 
 					// identification incorrecte
 				} else {
 					if (login == "" || password == "") {
-						if (login == "") {
-							MessageBox mb = new MessageBox(vLogin.getShlLogin(), SWT.OK | SWT.ICON_WARNING);
-							mb.setMessage("Veuillez remplir le login");
-							mb.open();
-						} else if (password == "") {
-							MessageBox mb = new MessageBox(vLogin.getShlLogin(), SWT.OK | SWT.ICON_WARNING);
-							mb.setMessage("Veuillez remplir le mot de passe");
-							mb.open();
+						vLogin.getLblmsgerreur().setText("Veuillez remplir tous les champs");
 						}
-					} else if (login != "" && password != "") {
-						MessageBox mb = new MessageBox(vLogin.getShlLogin(), SWT.OK | SWT.ICON_WARNING);
-						mb.setMessage("Identifiant incorrect");
-						mb.open();
+					else if(login != "" && password != "") {
+						vLogin.getLblmsgerreur().setText("Identifiant incorrecte");
 					}
 				}
-
 			}
 
 			@Override
@@ -108,17 +85,110 @@ public class LoginController implements SelectionListener {
 		});
 		// bouton annuler
 		vLogin.getBtnAnnuler().addSelectionListener(new SelectionListener() {
-
 			@Override
 			public void widgetSelected(SelectionEvent arg0) {
 				vLogin.getText_login().setText("");
 				vLogin.getText_mdp().setText("");
 			}
-
+			
 			@Override
 			public void widgetDefaultSelected(SelectionEvent arg0) {
 				// TODO Auto-generated method stub
+			}
+		});
+		
+		vLogin.getText_login().addKeyListener(new KeyListener() {
+			@Override
+			public void keyReleased(KeyEvent event) {
+				if (event.keyCode == 13) {
+					String login = vLogin.getText_login().getText();
+					String password = vLogin.getText_mdp().getText();
 
+					// identification correcte
+					if (LoginController.verify(login, password)) {
+						// fermeture de la bo√Æte de login
+						vLogin.getShlLogin().close();
+						AccueilController.vAccueil.getItemMonProfil().setText("Mon profil");
+						AccueilController.vAccueil.getItemConnexion().setText("DÈconnexion");
+						AccueilController.vAccueil.getItemMonProfil().setEnabled(true);
+						AccueilController.vAccueil.getItemLogin().setText(AppController.getActiveUser().getLogin());
+						AccueilController.vAccueil.getGrpProduits().setVisible(true);
+						if (AppController.getActiveUser().getAdministrator()) {
+							AccueilController.vAccueil.getBtnProducts().setVisible(true);
+							AccueilController.vAccueil.getBtnCollaborators().setVisible(true);
+							AccueilController.vAccueil.getGrpCollaborateurs().setVisible(true);
+						}
+						else {
+							AccueilController.vAccueil.getBtnProducts().setVisible(false);
+							AccueilController.vAccueil.getBtnCollaborators().setVisible(false);
+							AccueilController.vAccueil.getGrpCollaborateurs().setVisible(false);
+							AccueilController.vAccueil.getGrpCollaborateurs().setText("DÈtail du produit");
+							AccueilController.vAccueil.getTableMesProjets().setVisible(true);
+						}
+
+						// identification incorrecte
+					} else {
+						if (login == "" || password == "") {
+							vLogin.getLblmsgerreur().setText("Veuillez remplir tous les champs");
+							}
+						else if(login != "" && password != "") {
+							vLogin.getLblmsgerreur().setText("Identifiant incorrecte");
+						}
+					}
+				}
+			}
+			
+			@Override
+			public void keyPressed(KeyEvent arg0) {
+				// TODO Auto-generated method stub	
+			}
+		});
+		
+		vLogin.getText_mdp().addKeyListener(new KeyListener() {
+			@Override
+			public void keyReleased(KeyEvent event) {
+				if (event.keyCode == 13) {
+					String login = vLogin.getText_login().getText();
+					String password = vLogin.getText_mdp().getText();
+
+					// identification correcte
+					if (LoginController.verify(login, password)) {
+						// fermeture de la bo√Æte de login
+						vLogin.getShlLogin().close();
+						AccueilController.vAccueil.getItemMonProfil().setText("Mon profil");
+						AccueilController.vAccueil.getItemConnexion().setText("DÈconnexion");
+						AccueilController.vAccueil.getItemMonProfil().setEnabled(true);
+						AccueilController.vAccueil.getItemLogin().setText(AppController.getActiveUser().getLogin());
+						AccueilController.vAccueil.getGrpProduits().setVisible(true);
+						if (AppController.getActiveUser().getAdministrator()) {
+							AccueilController.vAccueil.getBtnProducts().setVisible(true);
+							AccueilController.vAccueil.getBtnCollaborators().setVisible(true);
+							AccueilController.vAccueil.getGrpCollaborateurs().setVisible(true);
+						}
+						else {
+							AccueilController.vAccueil.getBtnProducts().setVisible(false);
+							AccueilController.vAccueil.getBtnCollaborators().setVisible(false);
+							AccueilController.vAccueil.getGrpCollaborateurs().setVisible(false);
+							AccueilController.vAccueil.getGrpCollaborateurs().setText("DÈtail du produit");
+							AccueilController.vAccueil.getTableMesProjets().setVisible(true);
+						}
+
+						// identification incorrecte
+					} else {
+						if (login == "" || password == "") {
+							vLogin.getLblmsgerreur().setText("Veuillez remplir tous les champs");
+							}
+						else if(login != "" && password != "") {
+							vLogin.getLblmsgerreur().setText("Identifiant incorrecte");
+						}
+					}
+				}
+			}
+			
+			@Override
+			public void keyPressed(KeyEvent arg0) {
+				// TODO Auto-generated method stub
+				
 			}
 		});
 	}
